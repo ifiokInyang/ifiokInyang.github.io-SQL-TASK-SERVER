cd95d22fdf32423de27dff44dbeceb473e52279e
ifiokInyang <ifiok.inyang@decagon.dev>
2023-03-23 09:40:27 +0100

    resolved cors to allow request from the client

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express()
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(logger("dev"));
 app.use(express.json());
 app.use("/polling-unit", PollingUnitRoutes)
